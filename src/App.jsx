519b2a0031a454981e20f1948c54f20b5a262503
Harsha <harrysvallabhaneni.1@gmail.com>
2023-08-04 01:00:34 +0530

    add local storage support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import { NewToDoForm } from "./NewToDoForm";
 import { ToDoList } from "./ToDoList";
 
 export default function App() {
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState(() => {
+    const localValue = localStorage.getItem("ITEM")
+    if (localValue == null) return;
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEM", JSON.stringify(toDos));
+  }, [toDos])
 
   function addToDo(title) {
     setToDos((currentToDos) => {
